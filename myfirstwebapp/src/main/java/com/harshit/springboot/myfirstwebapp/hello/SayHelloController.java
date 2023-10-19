51cc2a987bc08b9085e715ce3d4e8d93e5704f55
Harshit22209 <119040511+Harshit22209@users.noreply.github.com>
2023-10-19 15:32:14 +0530

    Add files via upload

---
+++ myfirstwebapp/src/main/java/com/harshit/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,13 @@
+package com.harshit.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    @RequestMapping("/say-hello-jsp")
+    public String sayHello(){
+        return "sayHello";
+    }
+}
---
+++ myfirstwebapp/src/main/java/com/harshit/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.harshit.springboot.myfirstwebapp.security;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    //LDAP or Database
+    //In Memory
+
+    //InMemoryUserDetailsManager
+    //InMemoryUserDetailsManager(UserDetails... users)
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+
+        Function<String, String> passwordEncoder
+                = input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username("harshit")
+                .password("dummy")
+                .roles("USER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.formLogin(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(header -> header.frameOptions(frameOptions -> frameOptions.disable()));
+
+        return http.build();
+    }
+
+}
